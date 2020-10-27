03ba2d84d190985bc8171034196c1feaad675006
贾小燕 <448745259@qq.com>
2020-10-27 20:17:02 +0800

    xcodebuild详情基础库

---
+++ EMallProductDetailLib/EMallProductDetailLib/EMallProductDetailLib.h
@@ -0,0 +1,19 @@
+//
+//  EMallProductDetailLib.h
+//  EMallProductDetailLib
+//
+//  Created by 贾小燕 on 2020/10/27.
+//  Copyright © 2020 xy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EMallProductDetailLib.
+FOUNDATION_EXPORT double EMallProductDetailLibVersionNumber;
+
+//! Project version string for EMallProductDetailLib.
+FOUNDATION_EXPORT const unsigned char EMallProductDetailLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EMallProductDetailLib/PublicHeader.h>
+
+
